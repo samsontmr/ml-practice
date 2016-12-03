6c7295495d554fa1e2f5554b6b4faa90237a9416
Samson <samsontmr@users.noreply.github.com>
2016-12-03 12:30:23 +0800

    Perform knn

--- practice1.py
+++ practice1.py
@@ -1,6 +1,16 @@
 from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = load_iris()
 type(iris)
 
-print (iris.data)
+x = iris.data
+y = iris.target
+
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(x, y)
+
+x_new = [[5, 3, 6, 1], [3, 5, 4, 2]]
+
+print (knn.predict(x_new))
